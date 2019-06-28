1a604b7168f406c16887c98b49ccc7ed2d558845
Zach Nusbaum <zach@zdnmail.com>
2019-06-28 16:29:18 -0700

    show name

--- src/features/address-verification/VerificationForm.js
+++ src/features/address-verification/VerificationForm.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import {
+  bindActionCreators
+} from 'redux';
+import {
+  connect
+} from 'react-redux';
 import * as actions from './redux/actions';
-import {Form, Segment, Message} from 'semantic-ui-react'
+import {
+  Form,
+  Segment,
+  Message
+} from 'semantic-ui-react'
 import MaskedInput from 'react-text-mask'
 
 export class VerificationForm extends Component {
@@ -13,68 +23,102 @@ export class VerificationForm extends Component {
   };
 
   handleSubmit = (e) => {
-    const {code} = this.props.addressVerification
+    const {
+      code
+    } = this.props.addressVerification
     e.preventDefault();
-    this.props.actions.sendVerification({verification_code: code});
+    this.props.actions.sendVerification({
+      verification_code: code
+    });
   }
 
   render() {
-    return (
-      <div className="address-verification-verification-form">
-        <Message positive attached>
-          <Message.Header>Welcome!</Message.Header>
-          <p className="instructions">To complete your address verification, enter the verification code exactly as it appears on the letter you received in the mail:</p>
-        </Message>
-        <Segment attached>
-          <Form onSubmit={this.handleSubmit}>
-            <div className="field">
-              <label htmlFor="code">Verification Code</label>
-              <MaskedInput
-                name="code"
-                autoFocus={true}
-                value={this.props.addressVerification.code}
-                onChange={(e) => this.props.actions.changeCode(e.target.value)}
-                mask={[
-                  /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
-                  /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
-                  /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
-                  /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
-                  /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/
-                  ]}
-              />
-            </div>
-            <Form.Button disabled={this.props.addressVerification.sendVerificationPending} positive>Submit</Form.Button>
-          </Form>
-        </Segment>
-        {this.props.addressVerification.sendVerificationPending && <h2>Please wait...</h2>}
-        {!!this.props.addressVerification.response && <Message>
-          <p><strong>Status:</strong> {this.props.addressVerification.response['status'].split('_').join(' ')}</p>
-          <p><strong>Address:</strong> <br />
-          {this.props.addressVerification.response['address']['address_line1']} <br />
-          {this.props.addressVerification.response['address']['address_line2'] ? this.props.addressVerification.response['address']['address_line2'] + '</br>' : null}
-          {this.props.addressVerification.response['address']['address_city']}, {this.props.addressVerification.response['address']['address_state']} {this.props.addressVerification.response['address']['address_zip']}
-          </p>
-        </Message> }
-      </div>
-    );
-  }
-}
+      return ( <
+        div className = "address-verification-verification-form" >
+        <
+        Message positive attached >
+        <
+        Message.Header > Welcome! < /Message.Header> <
+        p className = "instructions" > To complete your address verification, enter the verification code exactly as it appears on the letter you received in the mail: < /p> <
+        /Message> <
+        Segment attached >
+        <
+        Form onSubmit = {
+          this.handleSubmit
+        } >
+        <
+        div className = "field" >
+        <
+        label htmlFor = "code" > Verification Code < /label> <
+        MaskedInput name = "code"
+        autoFocus = {
+          true
+        }
+        value = {
+          this.props.addressVerification.code
+        }
+        onChange = {
+          (e) => this.props.actions.changeCode(e.target.value)
+        }
+        mask = {
+          [
+            /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
+            /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
+            /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
+            /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, '-',
+            /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/, /[a-z0-9]/
+          ]
+        }
+        /> <
+        /div> <
+        Form.Button disabled = {
+          this.props.addressVerification.sendVerificationPending
+        }
+        positive > Submit < /Form.Button> <
+        /Form> <
+        /Segment> {
+          this.props.addressVerification.sendVerificationPending && < h2 > Please wait... < /h2>} {
+            !!this.props.addressVerification.response && < Message >
+              <
+              p > < strong > Status: < /strong> {this.props.addressVerification.response['status'].split('_').join(' ')}</p >
+              <
+              p > < strong > Address: < /strong> <br / > {
+                this.props.addressVerification.response['address']['name']
+              } < br / > {
+                this.props.addressVerification.response['address']['address_line1']
+              } < br / > {
+                this.props.addressVerification.response['address']['address_line2'] ? this.props.addressVerification.response['address']['address_line2'] + '</br>' : null
+              } {
+                this.props.addressVerification.response['address']['address_city']
+              }, {
+                this.props.addressVerification.response['address']['address_state']
+              } {
+                this.props.addressVerification.response['address']['address_zip']
+              } <
+              /p> <
+              /Message> } <
+              /div>
+          );
+        }
+      }
 
-/* istanbul ignore next */
-function mapStateToProps(state) {
-  return {
-    addressVerification: state.addressVerification,
-  };
-}
+      /* istanbul ignore next */
+      function mapStateToProps(state) {
+        return {
+          addressVerification: state.addressVerification,
+        };
+      }
 
-/* istanbul ignore next */
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators({ ...actions }, dispatch)
-  };
-}
+      /* istanbul ignore next */
+      function mapDispatchToProps(dispatch) {
+        return {
+          actions: bindActionCreators({
+            ...actions
+          }, dispatch)
+        };
+      }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(VerificationForm);
+      export default connect(
+        mapStateToProps,
+        mapDispatchToProps
+      )(VerificationForm);
